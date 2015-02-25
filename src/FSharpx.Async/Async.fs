a378ef6616e8ae4027a15a28f40feeac28dc7c3d
panesofglass <ryan.riley@panesofglass.org>
2015-02-25 09:28:52 -0600

    Fix spacing

--- src/FSharpx.Async/Async.fs
+++ src/FSharpx.Async/Async.fs
@@ -12,26 +12,26 @@ open System.Threading
 module AsyncExtensions = 
     type Microsoft.FSharp.Control.Async with 
 
-      /// Creates an asynchronous workflow that runs the asynchronous workflow
-      /// given as an argument at most once. When the returned workflow is 
-      /// started for the second time, it reuses the result of the 
-      /// previous execution.
-      static member Cache (input:Async<'T>) = 
-          let agent = Agent<AsyncReplyChannel<_>>.Start(fun agent -> async {
+    /// Creates an asynchronous workflow that runs the asynchronous workflow
+    /// given as an argument at most once. When the returned workflow is 
+    /// started for the second time, it reuses the result of the 
+    /// previous execution.
+    static member Cache (input:Async<'T>) = 
+      let agent = Agent<AsyncReplyChannel<_>>.Start(fun agent -> async {
+          let! repl = agent.Receive()
+          let! res = input
+          repl.Reply(res)
+          while true do
               let! repl = agent.Receive()
-              let! res = input
-              repl.Reply(res)
-              while true do
-                  let! repl = agent.Receive()
-                  repl.Reply(res) })
+              repl.Reply(res) })
 
-          async { return! agent.PostAndAsyncReply(id) }
+      async { return! agent.PostAndAsyncReply(id) }
 
-      /// Starts the specified operation using a new CancellationToken and returns
-      /// IDisposable object that cancels the computation. This method can be used
-      /// when implementing the Subscribe method of IObservable interface.
-      static member StartDisposable(op:Async<unit>) =
-          let ct = new System.Threading.CancellationTokenSource()
-          Async.Start(op, ct.Token)
-          { new IDisposable with 
-              member x.Dispose() = ct.Cancel() }
+    /// Starts the specified operation using a new CancellationToken and returns
+    /// IDisposable object that cancels the computation. This method can be used
+    /// when implementing the Subscribe method of IObservable interface.
+    static member StartDisposable(op:Async<unit>) =
+      let ct = new System.Threading.CancellationTokenSource()
+      Async.Start(op, ct.Token)
+      { new IDisposable with 
+          member x.Dispose() = ct.Cancel() }
