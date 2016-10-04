44bcf05b8c48cdb58a3156be6beb4dfd7cea83c7
Sudarshan Tanuku <mail@sudarshans.net>
2016-10-04 16:33:27 -0400

    issues #16 (migrated the samples from https://github.com/tpetricek/FSharp.AsyncExtensions/tree/master/samples to a Samples project. Also made the scripts work by adding required references and nuget packages)

---
+++ samples/FSharpx.Async.Samples/AutoCancel.fsx
@@ -0,0 +1,41 @@
+// ----------------------------------------------------------------------------
+// F# async extensions (AutoCancel.fsx)
+// (c) Tomas Petricek, 2011, Available under Apache 2.0 license.
+// ----------------------------------------------------------------------------
+
+// This example demonstrates how to use 'AutoCancelAgent'
+// The agent automatically stops its body when disposed.
+
+#r @"..\..\bin\FSharpx.Async.dll"
+
+open FSharpx.Control
+open System.Threading
+
+let op = async {
+  // Create a local agent that is disposed when the 
+  // workflow completes (using the 'use' construct)
+  use agent = AutoCancelAgent.Start(fun agent -> async { 
+    try 
+      while true do
+        // Wait for a message - note that we use timeout
+        // to allow cancellation (when the operation completes)
+        let! msg = agent.Receive(1000)
+        match msg with 
+        | (n, reply:AsyncReplyChannel<unit>) ->
+            // Print number and reply to the sender
+            printfn "%d (Thread# %A)" n Thread.CurrentThread.ManagedThreadId
+            reply.Reply(())        
+    finally 
+      // Called when the agent is disposed
+      printfn "agent completed" })
+  
+  // Do some processing using the agent...
+  for i in 0 .. 10 do 
+    do! Async.Sleep(100)
+    do! agent.PostAndAsyncReply(fun r -> i, r) 
+
+  do! Async.Sleep(100)
+  printfn "workflow completed (Thread# %A)" Thread.CurrentThread.ManagedThreadId}
+
+printfn "starting workflow (Thread# %A)" Thread.CurrentThread.ManagedThreadId
+Async.Start(op)
